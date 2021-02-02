52b980c998a115ee54c99648e0b54e37da08b74e
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-01 22:13:36 -0900

    Add tests: dead cells with 3 neighbors become alive

--- CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
+++ CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
@@ -23,8 +23,14 @@ TEST_CASE("A living cell with less than two neighbors: dies!") {
 	REQUIRE(gameOfLife("OXO\nXXX\nOXO") == "XXX\nXXX\nXXX");
 }
 
-TEST_CASE("A liveing cell with four neighbors: dies!") {
+TEST_CASE("A living cell with four neighbors: dies!") {
 	REQUIRE(gameOfLife("OOO\nOOO\nOOO") == "OOO\nOXO\nOOO");
 	REQUIRE(gameOfLife("XOX\nOOO\nXOX") == "XXX\nXXX\nXXX");
 	REQUIRE(gameOfLife("OOX\nOOO\nXOX") == "OOX\nOXX\nXXX");
+}
+
+TEST_CASE("A dead cell with exactly three neighbors: becomes alive!") {
+	REQUIRE(gameOfLife("OXO\nXOX\nXXX") == "XXX\nXOX\nXXX");
+	REQUIRE(gameOfLife("XOX\nOXO\nXXX") == "XXX\nXOX\nXXX");
+	REQUIRE(gameOfLife("OOO\nOOX\nOOO") == "OOO\nOXO\nOOO");
 }
