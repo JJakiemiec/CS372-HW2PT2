b030a37252da11e485448086d9e318d343dc7283
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-01 22:02:37 -0900

    Pass tests: cell with less than 2 neighbors dies

--- CS372 HW2PT2/CS372 HW2PT2/gameOfLife.cpp
+++ CS372 HW2PT2/CS372 HW2PT2/gameOfLife.cpp
@@ -5,6 +5,39 @@
 using std::string;
 #include <regex>
 using std::regex;
+#include <vector>
+using std::vector;
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+//count how many neighbors are the specified index are the specified character
+int neighborCount(const vector<vector<char>> & board, int row, int col, char c) {
+	int count = 0;
+	if (board[row + 1][col] == 'O')
+		count++;
+	if (board[row - 1][col] == 'O')
+		count++;
+	if (board[row][col + 1] == 'O')
+		count++;
+	if (board[row][col - 1] == 'O')
+		count++;
+	return count;
+}
+
+//print the board
+//used for testing
+void printboard(const vector<vector<char>>& board) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
 
 //gameOfLife
 //takes in a string which is a 3x3 of either X's or O's
@@ -12,28 +45,52 @@ using std::regex;
 //	OXO
 //	OOO
 //	OOO
+//
+//	O represents a living cell
+//	X represents a dead cell
 string gameOfLife(string board) {
-
-	/*bool format = true;
-	for (int i = 0; i < board.size(); i++) {
-		if ((i - 1) % 4 == 0) {
-			if (board[i] != '\n') {
-				format = false;
-			}
-		}
-		else
-			if (!(board[i] == 'O' || board[i] == 'X'))
-				format = false;
-
-
-	}
-	if (board == "")
-		format = false;
-
-	if (!format)
-		return "ERROR: incorrect board format";*/
+	//check to see if the board is formatted correctly
 	regex format("[OX][OX][OX]\n[OX][OX][OX]\n[OX][OX][OX]");
+	regex newline("\n");
 	if (!std::regex_match(board, format))
 		return "ERROR: incorrect board format";
-	return "";
+
+
+	//convert board into a more usable 2-d vector
+	//the board will be 5x5 to make boundary searching easier
+	//N (neutral) will be the boundary
+	board = board.substr(0, 3) + board.substr(4, 3) + board.substr(8, 3);
+	vector<vector<char>> boardArray(5, vector<char> (5, 'N'));
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			boardArray[i+1][j+1] = board[i * 3 + j];
+		}
+	}
+	vector<vector<char>> boardArrayCopy = boardArray;
+
+	printboard(boardArray);
+
+	//do the checks to see what happens to each cell
+	for (int i = 1; i < 4; i++) {
+		for (int j = 1; j < 4; j++) {
+			int alive = neighborCount(boardArray, i, j, 'O');
+			cout << alive;
+			if (alive < 2)
+				boardArrayCopy[i][j] = 'X';
+		}
+	}
+	cout << endl;
+	printboard(boardArrayCopy);
+
+	//convert the 2-d vector back into a string
+	string output = "";
+	for (int i = 1; i < 4; i++) {
+		for (int j = 1; j < 4; j++) {
+			output += boardArrayCopy[i][j];
+		}
+		output += '\n';
+	}
+	output.pop_back();
+
+	return output;
 }
