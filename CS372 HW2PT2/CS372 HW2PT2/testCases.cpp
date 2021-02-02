2dc79a8365fc22cd90ead72a27e2e4283e1ae9b9
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-01 20:52:27 -0900

    Add first tests: board formatted correctly

--- CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
+++ CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
@@ -5,3 +5,12 @@
 #include "catch.hpp"
 #include "gameOfLife.hpp"
 
+TEST_CASE("A wrongly formated board gives an error") {
+	REQUIRE("" == "ERROR: incorrect board format");
+	REQUIRE("asdf" == "ERROR: incorrect board format");
+	REQUIRE("ooo\nooo\nooo" == "ERROR: incorrect board format");
+	REQUIRE("OOO\nOOO\nOOo" == "ERROR: incorrect board format");
+	REQUIRE("XXX\nXXX\nXXx" == "ERROR: incorrect board format");
+	REQUIRE("XXX\nXXX\nXXX" != "ERROR: incorrect board format");
+	REQUIRE("OOO\nOOO\nOOO" != "ERROR: incorrect board format");
+}
