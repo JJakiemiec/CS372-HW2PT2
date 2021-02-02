ed6a3379b6598a49df028628c8df935c59210b21
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-01 21:16:48 -0900

    Give and error when board is formatted incorrectly

--- CS372 HW2PT2/CS372 HW2PT2/gameOfLife.cpp
+++ CS372 HW2PT2/CS372 HW2PT2/gameOfLife.cpp
@@ -3,6 +3,8 @@
 
 #include <string>
 using std::string;
+#include <regex>
+using std::regex;
 
 //gameOfLife
 //takes in a string which is a 3x3 of either X's or O's
@@ -12,5 +14,26 @@ using std::string;
 //	OOO
 string gameOfLife(string board) {
 
+	/*bool format = true;
+	for (int i = 0; i < board.size(); i++) {
+		if ((i - 1) % 4 == 0) {
+			if (board[i] != '\n') {
+				format = false;
+			}
+		}
+		else
+			if (!(board[i] == 'O' || board[i] == 'X'))
+				format = false;
+
+
+	}
+	if (board == "")
+		format = false;
+
+	if (!format)
+		return "ERROR: incorrect board format";*/
+	regex format("[OX][OX][OX]\n[OX][OX][OX]\n[OX][OX][OX]");
+	if (!std::regex_match(board, format))
+		return "ERROR: incorrect board format";
 	return "";
 }
