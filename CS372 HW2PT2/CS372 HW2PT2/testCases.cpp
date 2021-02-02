42bd296f4d1c7f25a9d161d71edede48847357c2
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-01 22:07:58 -0900

    Add tests: cell with 4 neighbors dies

--- CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
+++ CS372 HW2PT2/CS372 HW2PT2/testCases.cpp
@@ -17,10 +17,15 @@ TEST_CASE("A wrongly formated board gives an error") {
 }
 
 TEST_CASE("A living cell with less than two neighbors: dies!") {
-	REQUIRE(gameOfLife("OOO\nOOO\nOOO") == "OOO\nOOO\nOOO");
 	REQUIRE(gameOfLife("XXX\nXXX\nXXX") == "XXX\nXXX\nXXX");
 	REQUIRE(gameOfLife("XXX\nXOX\nXXX") == "XXX\nXXX\nXXX");
 	REQUIRE(gameOfLife("XOX\nOOO\nXOX") == "XXX\nXOX\nXXX");
 	REQUIRE(gameOfLife("OOO\nXXX\nXXX") == "XOX\nXXX\nXXX");
 	REQUIRE(gameOfLife("OXO\nXXX\nOXO") == "XXX\nXXX\nXXX");
+}
+
+TEST_CASE("A liveing cell with four neighbors: dies!") {
+	REQUIRE(gameOfLife("OOO\nOOO\nOOO") == "OOO\nOXO\nOOO");
+	REQUIRE(gameOfLife("XOX\nOOO\nXOX") == "XXX\nXXX\nXXX");
+	REQUIRE(gameOfLife("OOX\nOOO\nXOX") == "OOX\nOXX\nXXX");
 }
